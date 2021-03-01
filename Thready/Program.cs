9a08fa04ff35e1ef83e1497f48b735d5bfb5d2d9
Jakub Jancicka <jan0751@vsb.cz>
2021-03-01 17:27:21 +0100

    aaa

--- Thready/Program.cs
+++ Thready/Program.cs
@@ -1,54 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Runtime.InteropServices.ComTypes;
 using System.Threading;
+using Microsoft.VisualBasic;
 
-namespace Thready
+namespace Thready2
 {
     class Program
     {
-        static readonly object _lock = new object();
-        private static SimpleStack<int> simpleStack = new SimpleStack<int>();
         static void Main(string[] args)
         {
-            
+            object _lock = new object();
+            SimpleStack<int> simpleStack = new SimpleStack<int>();
 
             Random rand = new Random();
 
+
+            Thread t = new Thread((() =>
+            {
+                while (true)
+                {
+                    simpleStack.Push(rand.Next());
+                    lock (_lock)
+                    {
+                        Monitor.Pulse(_lock);
+                    }
+
+                    Thread.Sleep(100);
+                }
+            }));
+            t.Start();
+
+
             List<Thread> threads = new List<Thread>();
+
+            int id;
 
             for (int i = 0; i < 5; i++)
             {
-                Thread t = new Thread(CallThread);
-                t.Start(rand);
-                threads.Add(t);
-            }
-
-            foreach (var t in threads)
-            {
-                t.Join();
-            }
-
-            static void CallThread(object randGen)
-            {
-                Random rnd = (Random) randGen;
-
-                while (true)
+                Thread t2 = new Thread((() =>
                 {
-                    if (rnd.NextDouble() > 0.6)
-                    {
-                        simpleStack.Push(rnd.Next());
-                    }
-                    else
+                    while (true)
                     {
                         lock (_lock)
                         {
-                            if (!simpleStack.IsEmpty)
+                            if (simpleStack.IsEmpty)
                             {
-                                simpleStack.Pop();
+                                do
+                                {
+                                    Monitor.Wait(_lock);
+                                } while (simpleStack.IsEmpty);
                             }
+
+                            id = simpleStack.Pop(); 
                         }
+
+                        Console.WriteLine($"{id}, {Thread.CurrentThread.ManagedThreadId}");
+                        Thread.Sleep(rand.Next(40,1001));
                     }
-                }
+                }));
+                t2.Start();
+                threads.Add(t2);
+            }
+
+            t.Join();
+            foreach (var t2 in threads)
+            {
+                t2.Join();
             }
 
         }
--- Thready/SimpleStack.cs
+++ Thready/SimpleStack.cs
@@ -2,7 +2,7 @@
     using System;
     using System.Collections.Generic;
     
-    namespace Thready
+    namespace Thready2
 {
         public class SimpleStack<T>
         {
